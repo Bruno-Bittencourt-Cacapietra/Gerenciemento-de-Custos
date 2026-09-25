443c42ad28a0f83ca0c6fbf4e6eab78f1195fcb2
Bruno Bittencourt Caçapietra <bruno.cacapietra@edu.pucrs.br>
2026-09-25 03:08:34 -0300

    Criação da classe Departamento

---
+++ src/Departamento.java
@@ -0,0 +1,15 @@
+public class Departamento{
+    private String nome;
+    
+    public Departamento(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
