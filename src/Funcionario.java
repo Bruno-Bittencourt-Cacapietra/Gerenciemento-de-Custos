80e37375c7480cfb6c4d3798ca6364cebf3cbe43
Bruno Bittencourt Caçapietra <bruno.cacapietra@edu.pucrs.br>
2026-09-25 03:59:18 -0300

    Criação da classe Funcionário

---
+++ src/Funcionario.java
@@ -0,0 +1,32 @@
+public class Funcionario extends Operador {
+    private String matricula;
+    private Departamento departamento;
+
+    public Funcionario(String nome, String matricula, Departamento departamento){
+        super(nome);
+        this.matricula = matricula;
+        this.departamento = departamento;
+    }
+
+    public String getMatricula(){
+        return this.matricula;
+    }
+
+    public Departamento getDepartamento(){
+        return this.departamento;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public void setDepartamento(Departamento departamento){
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString(){
+        super.toString();
+        return "\nMatrícula: " + matricula + "\nDepartamento: " + departamento;
+    }
+}
